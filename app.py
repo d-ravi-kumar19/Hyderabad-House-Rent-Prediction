5ffceafdaaf7e175b5e2e77af7122a32d2caee54
d-ravi-kumar19 <dravikumar19@outlook.com>
2024-02-23 20:30:42 +0530

    updated app.py

--- app.py
+++ app.py
@@ -5,57 +5,35 @@ import pickle
 
 
 cleaned_df = pd.read_csv('data/cleaned_data.csv')
-X = cleaned_df.drop(['rent_amount'], axis=1)
+X = cleaned_df.drop(['rent_amount','localityId'], axis=1)
 
-# Load the trained model
 with open('hyd_house_rent_prices.pkl', 'rb') as model_file:
     best_model = pickle.load(model_file)
 
-
 def reverse_type_bhk_encoding(value):
-    d = {0: "RK1", 1: 'BHK1', 2: 'BHK2', 3: 'BHK3', 4: 'BHK4', 5: 'BHK4PLUS'}
+    d = {0.5: "RK1", 1: 'BHK1', 2: 'BHK2', 3: 'BHK3', 4: 'BHK4', 5: 'BHK4PLUS'}
     return d.get(value)
-
 
 def reverse_parking_encoding(value):
-    d = {0: "None", 1: '3 wheeler', 2: '4 wheele', 3: 'Both'}
+    d = {0: "None", 1: '2 wheeler', 2: '4 wheeler', 3: 'Both'}
     return d.get(value)
-
 
 def reverse_lift_encoding(value):
     d = {0: "No", 1: 'Yes'}
     return d.get(value)
 
-
 def reverse_furnishing_encoding(value):
     d = {0.5: "Semi funrnished", 1: 'Fully furnished', 0: 'Unfurnished'}
     return d.get(value)
 
-
-def preprocess_input(localityId, type_bhk, balconies, bathroom, parking, lift, furnishingDesc, property_size, maintenance):
-    input_data = pd.DataFrame({
-        'localityId': [localityId],
-        'type_bhk': [type_bhk],
-        'balconies': [balconies],
-        'bathroom': [bathroom],
-        'parking': [parking],
-        'lift': [lift],
-        'furnishingDesc': [furnishingDesc],
-        'property_size': [property_size],
-        'maintenance': [maintenance]
-    })
-
-    return input_data
-
-
-def predict_price(localityId, type_bhk, balconies, bathroom, parking, lift, furnishingDesc, property_size, maintenance):
+def predict_price(localityId, selected_type_bhk, selected_balconies, bathroom, parking, lift, furnishingDesc, property_size, maintenance):
     loc_index = np.where(X.columns == localityId)[0]
 
-    if loc_index.size > 0:
+    if loc_index:
         loc_index = loc_index[0]
         x = np.zeros(len(X.columns))
-        x[0] = type_bhk
-        x[1] = balconies
+        x[0] = selected_type_bhk
+        x[1] = selected_balconies
         x[2] = bathroom
         x[3] = parking
         x[4] = lift
@@ -70,44 +48,38 @@ def predict_price(localityId, type_bhk, balconies, bathroom, parking, lift, furn
     else:
         return "LocalityId not found in training data."
 
-  
-
 
 st.title('Hyderabad House Rent Prediction')
 locality_options = cleaned_df['localityId'].unique()
-selected_locality = st.selectbox('Locality', locality_options)
-type_bhk_options = cleaned_df['type_bhk'].unique()
-selected_type_bhk = st.selectbox('Type BHK', type_bhk_options, format_func=reverse_type_bhk_encoding)
-selected_balconies = st.number_input('Balconies', min_value=0, value=1)
-bathroom = st.number_input('Bathroom', min_value=1, value=1, max_value=5)
-parking_options = cleaned_df['parking'].unique()
-parking = st.selectbox('Parking', parking_options, format_func=reverse_parking_encoding)
+localityId = st.selectbox('Locality', locality_options) # localityId
+
+type_bhk_options = cleaned_df['type_bhk'].unique()   
+selected_type_bhk = st.selectbox('Type BHK', type_bhk_options, format_func=reverse_type_bhk_encoding)  # type_bhk
+
+selected_balconies = st.number_input('Balconies', min_value=0, value=1)                                 # bolconies
+
+bathroom = st.number_input('Bathroom', min_value=1, value=1, max_value=5)                               # bathrooms
+
+parking_options = cleaned_df['parking'].unique()                                                        
+parking = st.selectbox('Parking', parking_options, format_func=reverse_parking_encoding)                # parking
+
 lift_options = cleaned_df['lift'].unique()
-lift = st.selectbox('Lift', lift_options, format_func=reverse_lift_encoding)
+lift = st.selectbox('Lift', lift_options, format_func=reverse_lift_encoding)                            # lift
+
 furnishing_options = cleaned_df['furnishingDesc'].unique()
-furnishingDesc = st.selectbox('Furnishing', furnishing_options, format_func=reverse_furnishing_encoding)
-property_size = st.number_input('Property Size', min_value=1, value=1000)
-maintenance = st.number_input('Maintenance', min_value=1000, value=1000)
+furnishingDesc = st.selectbox('Furnishing', furnishing_options, format_func=reverse_furnishing_encoding)  # furnishing
+
+property_size = st.number_input('Property Size', min_value=1, value=1000)                                   # property size
+
+maintenance = st.number_input('Maintenance', min_value=1000, value=1000)                                    # maintenance
 
 if st.button('Predict Rent'):
-    # Check if selected locality is in the columns
-    if selected_locality in X.columns:
-        st.write(f"Selected Locality: {selected_locality}")
-        st.write(f"X Columns: {X.columns}")
-        loc_index = np.where(X.columns == selected_locality)[0][0]
-        x = np.zeros(len(X.columns))
-        input_data = preprocess_input(
-            selected_locality, selected_type_bhk, selected_balconies, bathroom,
-            parking, lift, furnishingDesc, property_size, maintenance
-        )
-
-        # Call the predict_price function
-        prediction_result = predict_price(best_model, input_data)
-
-        # Display the prediction
-        st.success(f'Predicted Rent: {float(prediction_result):.2f} INR')
+    if localityId in X.columns:
+        prediction_result = predict_price(localityId, selected_type_bhk, selected_balconies, bathroom,
+                                    parking, lift, furnishingDesc, property_size, maintenance)
+        st.success(f'Predicted Rent:  {float(prediction_result):.2f} INR')
     else:
-        st.error(f'Selected Locality "{selected_locality}" not found in training data.')
+        st.error(f'Selected Locality "{localityId}" not found in training data.')
 
 
 
